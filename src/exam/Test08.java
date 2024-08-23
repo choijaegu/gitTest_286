e80fdac7a0b386b985699b070f7031d13c062fe0
GNUSNUY\Gnusnuy <fun9908@naver.com>
2024-08-23 10:49:51 +0900

    Test중입니다

---
+++ src/exam/Test08.java
@@ -0,0 +1,8 @@
+package exam;
+
+public class Test08 {
+	public Test08() {
+		System.out.println("점심은 맥날");
+	}
+
+}
---
+++ src/exam/Test09.java
@@ -0,0 +1,14 @@
+package exam;
+
+public class Test09 
+{
+	public Test09()
+	{
+		
+		
+		
+		
+	}
+	
+	
+}
